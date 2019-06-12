1f6c85a27129786926481fa5f9817ab045fe270e
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-12 12:09:18 -0300

    dependência mongoose adicionada ao package.json e aplicação conectada ao Mongo DB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://semana:semana@cluster0-s23yo.mongodb.net/test?retryWrites=true&w=majority', {
+   useNewUrlParser: true,
+});
 
 app.get('/', (req, res) => {
    return res.send(`Olá ${req.query.name}`);
