94051b68511ad37c201c9ca7eb591f6f648708f8
Felipe Pereira <pereira_felipesilva@outlook.com>
2019-06-12 14:09:37 -0300

    implementação do resize com sharp

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -14,6 +14,15 @@ module.exports = {
       const { author, place, description, hashtags } = req.body;
       const { filename: image } = req.file;
 
+      await sharp(req.file.path)
+         .resize(500)
+         .jpeg({ quality: 70 })
+         .toFile(
+            path.resolve(req.file.destination, 'resized', image)
+         )
+
+      fs.unlinkSync(req.file.path);
+
       const post = await Post.create({
          author,
          place,
